3cb54bca291e563b95aebd8ad579c9d403b74856
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-01-15 13:28:45 -0300

    Add basic Vim opts, keymaps and colorscheme.

---
+++ lua/colorscheme.lua
@@ -0,0 +1,15 @@
+--[[ Colorscheme options.
+--
+-- Requires the colorscheme to be installed, i.e. in */site/pack/*/start/*
+-- I'm using "Catppuccin" (https://github.com/catppuccin/nvim) without a plugin manager.
+-- (Believe me, you don't need it)
+--
+-- I always match my terminal colorscheme with nvim's, so transparent backgrounds work great
+-- (And look very nice indeed!).
+--]]
+
+require "catppuccin".setup { transparent_background = true }
+
+vim.cmd.colorscheme "catppuccin-frappe"
+
+return {}
---
+++ lua/keymaps.lua
@@ -0,0 +1,21 @@
+--[[ Keybindings for Neovim
+--]]
+
+local setkey = vim.keymap.set
+
+keybindings = {
+	-- Personal preference:
+	{ mode = 'n', key = '<C-z>', cmd = ':w<CR>', opts = { desc = 'Nano::Write' } },
+	{ mode = 'i', key = '<C-z>', cmd = '<ESC>:w<CR>', opts = { desc = 'Nano::Write' } },
+	{ mode = 'n', key = '<ESC>n', cmd = ':tabnew<CR>', opts = { desc = 'Nano::NewTab' } },
+	{ mode = 'n', key = '<ESC>q', cmd = ':q<CR>', opts = { desc = 'Nano::Quit' } },
+	{ mode = 'n', key = '<ESC>Q', cmd = ':qa<CR>', opts = { desc = 'Nano::QuitAll' } },
+	{ mode = 'n', key = '<ESC>o', cmd = ':only<CR>', opts = { desc = 'Nano::Only' } },
+	{ mode = 'n', key = '<ESC>w', cmd = ':tabclose<CR>', opts = { desc = 'Nano::CloseTab' } },
+}
+
+for _, kb in pairs(keybindings) do
+	setkey(kb.mode, kb.key, kb.cmd, kb.opts)
+end
+
+return {}
