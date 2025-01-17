2c5fbe27122ea44a6768b583a5354bdc6e74edfe
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-01-17 13:31:14 -0300

    Add command to Stop/Restart the LS.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -19,34 +19,59 @@ vim.diagnostic.config({
 	float = { border = border_style }
 })
 
+local function start_gopls()
+	vim.lsp.start({
+		name = 'Nano_gopls',
+		cmd = { 'gopls' },
+		root_dir = vim.fs.root(0, { 'go.mod' }),
+	})
+end
+
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'go',
-	callback = function(x)
-		vim.lsp.start({
-			name = 'Nano::gopls',
-			cmd = { 'gopls' },
-			root_dir = vim.fs.root(x.buf, { 'go.mod' }),
-		})
-	end,
+	callback = start_gopls,
 })
+
+local function start_lua_ls()
+	local command = { 'lua-language-server' }
+	local root = vim.fs.dirname(vim.fs.find({ '.git', 'nvim' }, { upward = true })[1])
+	if root == '~/.config/nvim' then
+		table.insert(command, '--configpath')
+		table.insert(command, '~/.config/nvim/.luarc.json')
+	end
+	vim.lsp.start({
+		name = 'Nano_lua_ls',
+		cmd = command,
+		root_dir = root,
+		workspace_folders = nil,
+		-- Settings are now on .luarc.json (And it works!).
+		-- settings = { ... }
+	})
+end
+
+local function stop_ls()
+	vim.lsp.stop_client(vim.lsp.get_clients())
+end
+
+local function restart_ls()
+	stop_ls()
+	local ft = vim.filetype.match({buf = 0})
+	if ft == 'go' then
+		start_gopls()
+	elseif ft == 'lua' then
+		start_lua_ls()
+	end
+end
 
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'lua',
-	callback = function(fname)
-		vim.lsp.start({
-			name = 'Nano_lua_ls',
-			-- TODO: Add Neovim's libraries conditionally only when needed.
-			cmd = { 'lua-language-server', '--configpath', '~/.config/nvim/.luarc.json' },
-			root_dir = vim.fs.dirname(vim.fs.find({ '.git', 'nvim' }, { upward = true })[1]),
-			workspace_folders = nil,
-			-- Settings are now on .luarc.json (And it works!).
-			-- settings = { ... }
-		})
-	end,
+	callback = start_lua_ls,
 })
 
 -- Some commands.
 vim.api.nvim_create_user_command('LspCheck', ':vert checkhealth lsp', { desc = 'Nano:: LSP Info' })
+vim.api.nvim_create_user_command('LspStop', stop_ls, { desc = 'Nano:: LSP Info' })
+vim.api.nvim_create_user_command('LspRestart', restart_ls, { desc = 'Nano:: LSP Info' })
 
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'checkhealth',
