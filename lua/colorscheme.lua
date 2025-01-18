72fec49042260121b0bb8c84141833e56583f988
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-01-18 16:22:44 -0300

    Customise colours for Pmenu so it stands out from the rest.

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -11,4 +11,25 @@ require "catppuccin".setup { transparent_background = true }
 
 vim.cmd.colorscheme "catppuccin-frappe"
 
+-- Customise the popup menu.
+local item = { bg = "#232635", fg = "#737995" }
+local item_sel = { bg = "#232635", fg = "#c6d0f6" }
+local kind = { bg = "#232635", fg = "#ca9ee7" }
+local kind_sel = { bg = "#232635", fg = "#f4b8e5" }
+local extra = { bg = "#232635", fg = "#737995" }
+local extra_sel = { bg = "#232635", fg = "#c6d0f6" }
+local sbar = { bg = "#111317" }
+local thumb = { bg = "#343951" }
+
+vim.cmd.highlight({ args = { "Pmenu", "guibg=" .. item.bg, "guifg=" .. item.fg } })
+vim.cmd.highlight({ args = { "PmenuSel", "guibg=" .. item_sel.bg, "guifg=" .. item_sel.fg } })
+vim.cmd.highlight({ args = { "PmenuKind", "gui=italic", "guibg=" .. kind.bg, "guifg=" .. kind.fg } })
+vim.cmd.highlight({ args = { "PmenuKindSel", "gui=italic", "guibg=" .. kind_sel.bg, "guifg=" .. kind_sel.fg } })
+vim.cmd.highlight({ args = { "PmenuExtra", "guibg=" .. extra.bg, "guifg=" .. extra.fg } })
+vim.cmd.highlight({ args = { "PmenuExtraSel", "guibg=" .. extra_sel.bg, "guifg=" .. extra_sel.fg } })
+vim.cmd.highlight({ args = { "PmenuSbar", "guibg=" .. sbar.bg } })
+vim.cmd.highlight({ args = { "PmenuThumb", "guibg=" .. thumb.bg } })
+vim.cmd.highlight({ args = { "NormalFloat", "guibg=" .. item.bg, "guifg=" .. item_sel.fg } })
+
+
 return {}
